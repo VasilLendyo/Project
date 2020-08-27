474bb4568ed1c2bd83ed209014a8a5ff8d5f90a2
LogosItAcademy <Tester99037@gmail.com>
2020-08-27 13:27:58 +0300

    delete models

--- selectionCommittee/src/main/java/ua/lviv/lgs/selectionCommittee/dao/DataRepository.java
+++
@@ -1,9 +0,0 @@
-package ua.lviv.lgs.selectionCommittee.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ua.lviv.lgs.selectionCommittee.domain.Data;
-
-public interface DataRepository extends JpaRepository<Data, Integer>{
-
-}
