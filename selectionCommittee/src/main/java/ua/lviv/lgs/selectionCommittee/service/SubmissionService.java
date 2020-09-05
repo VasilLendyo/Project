4db2718d9f7ca24e9b10fdc2e886c67e75f14c9d
LogosItAcademy <Tester99037@gmail.com>
2020-09-05 18:43:26 +0300

    added loggers to Service methods

--- selectionCommittee/src/main/java/ua/lviv/lgs/selectionCommittee/service/SubmissionService.java
+++ selectionCommittee/src/main/java/ua/lviv/lgs/selectionCommittee/service/SubmissionService.java
@@ -2,6 +2,8 @@ package ua.lviv.lgs.selectionCommittee.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,17 +12,21 @@ import ua.lviv.lgs.selectionCommittee.domain.Submission;
 
 @Service
 public class SubmissionService {
+	
+	Logger logger = LoggerFactory.getLogger(SubmissionService.class);
 
 	@Autowired
 	private SubmissionRepository submissionRepository;
 	
 
 	public List<Submission> getAll() {
+		logger.info("Get all applicants of submission");
 		return submissionRepository.findAll();
 	}
 	
 
 	public Submission add(Submission submission) {
+		logger.info("Register new applicant to submission");
 		return submissionRepository.save(submission);
 	}
 	
